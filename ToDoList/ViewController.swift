8cc260a524a3d0912be682be068eb16a30ae0845
Olivia Dugger <oliviadugger@Olivias-MacBook-Air.local>
2019-06-24 10:46:25 -0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Olivia Dugger on 6/24/19.
+//  Copyright © 2019 Olivia Dugger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
